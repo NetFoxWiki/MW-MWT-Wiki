ae69075648559d00b157f81e478edb97eca7ddd9
_布咕咕 <yuansepailin@163.com>
2023-11-05 18:30:03 +0800

    Update op.js

--- op.js
+++ op.js
@@ -64,7 +64,7 @@ const elm_audio_2 = document.createElement("audio");
         // 起洞！
         setTimeout(() => {
 			//此处已更改
-            window.location.href='https://vdse.bdstatic.com/192d9a98d782d9c74c96f09db9378d93.mp4';
+            window.location.href='https://mwdocs.info/DE.html';
             elm_div.onclick = () => {
                 elm_audio.pause();
                 elm_div.remove();
