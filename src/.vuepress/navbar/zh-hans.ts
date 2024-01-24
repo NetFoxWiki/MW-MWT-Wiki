58db3c4d72456fb64a8a9c3f57c44858d312e76a
网狐社 <yuansepailin@163.com>
2024-01-24 20:56:29 +0800

    mw

--- src/.vuepress/navbar/zh-hans.ts
+++ src/.vuepress/navbar/zh-hans.ts
@@ -10,13 +10,6 @@ export const zhhansNavbar = navbar([
     ],
   },
   {
-    text: "友情链接",
-    icon: "link",
-    children: [
-      { text: "舰队导航", icon: "navigation", link: "http://www.sss9.top/" },
-    ],
-  },
-  {
     text: "国区社区",
     icon: "flow",
     children: [
--- src/.vuepress/navbar/zh-hant.ts
+++ src/.vuepress/navbar/zh-hant.ts
@@ -9,35 +9,4 @@ export const zhhantNavbar = navbar([
       { text: "QQ", icon: "light", link: "https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=O_uVziCfUhzQseq9rCrjxwayDJDffOo9&authKey=ervLfyvDdYRlPRA%2FLK7NStIJgj23OlOZ1SyGa3aOd1XMUmUEsbW7iRi7mfrELOLX&noverify=0&group_code=421836260" },
     ],
   },
-  {
-    text: "友情連結",
-    icon: "link",
-    children: [
-      { text: "艦隊導航", icon: "navigation", link: "http://www.sss9.top/" },
-    ],
-  },
-  {
-    text: "國區社區",
-    icon: "flow",
-    children: [
-      {
-        text: "現代戰艦前沿新聞",
-        children: [
-          { text: "QQ群組", icon: "repo", link: "https://qm.qq.com/q/JqVFw6yogI" },
-        ],
-      },
-      {
-        text: "現代戰艦官方社區",
-        children: [
-          { text: "QQ頻道", icon: "define", link: "https://pd.qq.com/s/f0oylihze" },
-        ],
-      },
-      {
-        text: "現代戰艦玩家社區",
-        children: [
-          { text: "QQ頻道", icon: "blog", link: "https://pd.qq.com/s/8otd5z76x" },
-        ],
-      },
-    ],
-  },
 ]);
