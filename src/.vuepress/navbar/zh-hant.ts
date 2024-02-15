d34981bb73a6397f69d26011a5054073b6d03bcd
网狐社 <yuansepailin@163.com>
2024-02-15 13:13:58 +0800

    更新 zh-hant.ts

--- src/.vuepress/navbar/zh-hant.ts
+++ src/.vuepress/navbar/zh-hant.ts
@@ -12,4 +12,23 @@ export const zhhantNavbar = navbar([
       { text: "申請編輯", icon: "editor", link: "https://docs.qq.com/form/page/DUEJaclBuQ1N3SGtn" },
     ],
   },
+  {
+    text: "國區社區",
+    icon: "flow",
+    children: [
+      {
+        text: "現代戰艦前沿新聞",
+        children: [
+          { text: "QQ群組", icon: "share", link: "https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=wOQ2uH3_2YaXwM_aBAepGBfRTNPEwGjp&authKey=kcKqka8bkrpLWyhk%2BA2MNGZBgrmdNP8l6fDkUP%2FkbG5bLUEsQ5VsJ6sF5wVy1OGf&noverify=0&group_code=480946659" },
+        ],
+      },
+      {
+        text: "現代戰艦官方社區",
+        children: [
+          { text: "Fanbook", icon: "repo", link: "https://in.fanbook.cn/m8XB6FN7" },
+          { text: "QQ頻道", icon: "define", link: "https://pd.qq.com/s/f0oylihze" },
+        ],
+      },
+    ],
+  },
 ]);
