4351111e999e995161a1999b41a7df21a01cae01
网狐社 <yuansepailin@163.com>
2024-03-03 10:46:56 +0800

    m

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,12 +1,16 @@
 import { defineClientConfig } from "vuepress/client";
 import "vuepress-theme-hope/presets/bounce-icon.scss";
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
+import General from "./components/General.vue";
 //import { onMounted } from 'vue';
 //import { defineAsyncComponent } from 'vue';
 
 //const NavMusic = defineAsyncComponent(() => import('./components/NavMusic.vue'));
 
 export default defineClientConfig({
+	enhance: ({ app, router, siteData }) => {
+    app.component("General", General);
+  },
 	/*setup() {
     onMounted(() => {});
   },
---
+++ src/.vuepress/components/General.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="article-licensing">
+    <b>用于记录 <font color="#2196F3">现代战舰</font> 与 <font color="#fb8b05">现代战争：坦克</font> 内容的文档网站 🍂</b>
+  </div>
+</template>
+
+<style>
+.article-licensing {
+  padding: 1rem;
+  position: relative;
+  z-index: 1;
+  background: #f0f0f09a;
+  overflow: hidden;
+}
+
+.article-licensing:after {
+  position: absolute;
+  z-index: -1;
+  right: 20px;
+  top: -5px;
+  content: url("/gm.png");
+  opacity: 0.3;
+}
+</style>
