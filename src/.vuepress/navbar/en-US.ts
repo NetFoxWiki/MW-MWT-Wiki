cf3ac21df8307f71cbf8e866bf4e2b60aee6efad
网狐社 <yuansepailin@163.com>
2024-05-03 23:12:20 +0800

    隐藏 Fanbook 链接

--- src/.vuepress/navbar/en-US.ts
+++ src/.vuepress/navbar/en-US.ts
@@ -8,7 +8,6 @@ export const enUSNavbar = navbar([
       {
         text: "Contact/Discussion/Cooperation",
         children: [
-          { text: "Fanbook", icon: "repo", link: "https://in.fanbook.cn/DeIe6FCe" },
           { text: "Bilibili", icon: "at", link: "https://space.bilibili.com/1093209533" },
           { text: "QQ Groups", icon: "light", link: "https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=O_uVziCfUhzQseq9rCrjxwayDJDffOo9&authKey=ervLfyvDdYRlPRA%2FLK7NStIJgj23OlOZ1SyGa3aOd1XMUmUEsbW7iRi7mfrELOLX&noverify=0&group_code=421836260" },
         ],
--- src/.vuepress/navbar/zh-CN.ts
+++ src/.vuepress/navbar/zh-CN.ts
@@ -8,7 +8,7 @@ export const zhCNNavbar = navbar([
       {
         text: "联系/讨论/合作",
         children: [
-          { text: "Fanbook", icon: "repo", link: "https://in.fanbook.cn/DeIe6FCe" },
+          //{ text: "Fanbook", icon: "repo", link: "https://in.fanbook.cn/DeIe6FCe" },
           { text: "Bilibili", icon: "at", link: "https://space.bilibili.com/1093209533" },
           { text: "QQ 群组", icon: "light", link: "https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=O_uVziCfUhzQseq9rCrjxwayDJDffOo9&authKey=ervLfyvDdYRlPRA%2FLK7NStIJgj23OlOZ1SyGa3aOd1XMUmUEsbW7iRi7mfrELOLX&noverify=0&group_code=421836260" },
         ],
