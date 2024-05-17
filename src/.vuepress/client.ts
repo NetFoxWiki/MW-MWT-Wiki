8a9b61187a5f3a77a491c7fc64ae14b43c645f5b
网狐社 <yuansepailin@163.com>
2024-05-17 20:21:33 +0800

    [Add] Layouts + ISSUE

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -2,9 +2,13 @@ import { defineClientConfig } from "vuepress/client";
 import "vuepress-theme-hope/presets/bounce-icon.scss";
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
 import MWCard from "./components/MWCard.vue";
+//import NotFound from "./layouts/404NotFound.vue";
 
 export default defineClientConfig({
   enhance: ({ app }) => {
     app.component("MWCard", MWCard);
+  },
+  layouts: {
+    //NotFound,
   },
 });
