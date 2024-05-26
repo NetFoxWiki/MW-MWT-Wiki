90b42ab2487bdeb5f730af1b4ef37e3a088f7dea
网狐社 <yuansepailin@163.com>
2024-05-26 20:55:29 +0800

    [Apair] 样式更改

--- src/.vuepress/components/AD.vue
+++ src/.vuepress/components/AD.vue
@@ -16,9 +16,13 @@ import NetFoxWiki from "vuepress-theme-hope/modules/sidebar/components/Sidebar.j
             <div class="ad-main">
                 <a href="https://pd.qq.com/s/h27dr0db3">
                     <img class="ad-banner" src="https://statics.netfox.wiki/20240512/channels4_banner.1zi2t75wov.webp">
-                    <div class="ad-text">
-                        <small><b>MWT: Tank Battles 官方频道 - 点击加入</b></small>
-                    </div>
+                    <b class="ad-text">
+                        MWT: Tank Battles 官方频道
+                    </b>
+                    <br>
+                    <b class="ad-join">
+                        点击加入
+                    </b>
                 </a>
             </div>
         </template>
@@ -31,22 +35,33 @@ import NetFoxWiki from "vuepress-theme-hope/modules/sidebar/components/Sidebar.j
 }
 
 .ad-main {
+    box-shadow: 0 0 2px var(--theme-color);
+    border-bottom: 10px var(--theme-color-dark) solid;
+    border-radius: 5px;
     margin: -15px 10px 25px 10px;
+    padding: 2px 2px 5px 2px;
     text-align: center;
+}
+
+.ad-text {
+    color: var(--theme-color);
+    font-size: 17px;
+}
+
+.ad-join {
+    background-color: var(--theme-color);
+    border-radius: 5px;
+    padding: 1px;
+    font-size: 20px;
+    color: #ffffff;
+}
+
+.ad-main:hover {
+    box-shadow: 0 0 5px var(--theme-color);
 }
 
 .ad-banner {
     width: 100%;
-}
-
-.ad-text:hover {
-    background-color: #294167;
-    color: #ffffff;
-}
-
-.ad-text {
-    background-color: #bbd6e1;
-    margin-top: -5px;
-    color: #294167;
+    border-radius: 5px;
 }
 </style>
