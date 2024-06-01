fe1dfe2d6bd53948877c0b61a27beeddd17ae213
网狐社 <yuansepailin@163.com>
2024-06-01 12:42:28 +0800

    [Apair] 删除 English

--- src/.vuepress/navbar/en-US.ts
+++ src/.vuepress/navbar/en-US.ts
@@ -1,25 +1,4 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const enUSNavbar = navbar([
-  {
-    text: "Contact Us",
-    icon: "blog",
-    children: [
-      {
-        text: "Contact/Discussion/Cooperation",
-        children: [
-          { text: "Bilibili", icon: "at", link: "https://space.bilibili.com/1093209533" },
-          { text: "QQ Groups", icon: "comment", link: "https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=O_uVziCfUhzQseq9rCrjxwayDJDffOo9&authKey=ervLfyvDdYRlPRA%2FLK7NStIJgj23OlOZ1SyGa3aOd1XMUmUEsbW7iRi7mfrELOLX&noverify=0&group_code=421836260" },
-        ],
-      },
-      {
-        text: "Site Support",
-        children: [
-          { text: "Apply for editing", icon: "edit", link: "https://docs.qq.com/form/page/DUEJaclBuQ1N3SGtn" },
-          { text: "Site Feedback", icon: "bug", link: "https://docs.qq.com/form/page/DUFBXekFaUFZFSWJQ" },
-          { text: "Sponsored Sites", icon: "paper-plane", link: "https://afdian.net/a/netfoxqwq" },
-        ],
-      },
-    ],
-  },
 ]);
--- src/.vuepress/sidebar/en-US.ts
+++ src/.vuepress/sidebar/en-US.ts
@@ -1,9 +1,4 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const enUSSidebar = sidebar([
-	{
-		text: 'Welcome to MW & MWT Docs',
-		icon: "home",
-		link: 'en/index.md',
-	},
 ]);
