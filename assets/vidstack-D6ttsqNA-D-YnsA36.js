15b81b13eeb26d1fa081ed1b9f35c67b456c1f28
NetFoxQwQ <110319858+NetFoxQwQ@users.noreply.github.com>
2024-07-09 01:33:46 +0000

    Deploying to main from @ NetFoxWiki/MW-MWT-DOCS@d2fd07a1de0b9a692880f3f3a1a30547836e1485 🚀

--- assets/vidstack-D6ttsqNA-D-YnsA36.js
+++
@@ -1,1 +0,0 @@
-import{bW as i,bX as t}from"./app-Oo_vFapH.js";class n{constructor(s){this.La=s}Xa(){i(this.ya)&&this.fg()}$(){t(this.ya)&&window.cancelAnimationFrame(this.ya),this.ya=void 0}fg(){this.ya=window.requestAnimationFrame(()=>{i(this.ya)||(this.La(),this.fg())})}}export{n as R};
